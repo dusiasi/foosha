e8da80f20c5944abc025af906709e677bccf854a
Nadav <nadavberenstain@gmail.com>
2024-03-07 17:59:02 +0100

    typeScript update

--- client/src/services/utils.ts
+++ client/src/services/utils.ts
@@ -1,22 +1,21 @@
-
 // format date for items
-export function formatDate (dateString) {
+export function formatDate(dateString: Date) {
   const date = new Date(dateString);
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  return `${day}/${month}/${year}`
+  return `${day}/${month}/${year}`;
 }
 
 // format date time for messages and conversations
-export function formatDateTime (dateString) {
+export function formatDateTime(dateString:Date) {
   const date = new Date(dateString);
-  const hour = String(date.getHours()).padStart(2,0);
-  const min = String(date.getMinutes()).padStart(2,0);
+  const hour = String(date.getHours()).padStart(2, '0');
+  const min = String(date.getMinutes()).padStart(2, '0');
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  return `${day}/${month}/${year} - ${hour}:${min}`
+  return `${day}/${month}/${year} - ${hour}:${min}`;
 }
 
 // sort data arrays by date
@@ -29,21 +28,27 @@ export function sortByDate(array, dateProperty) {
 }
 
 // helper function
-function degreesToRadians(degrees) {
+function degreesToRadians(degrees: number) {
   return degrees * (Math.PI / 180);
 }
 
 // calculate distance between two locations
-export function calculateDistance (lat1, lng1, lat2, lng2) {
+export function calculateDistance(
+  lat1: number,
+  lng1: number,
+  lat2: number,
+  lng2: number
+) {
   const R = 6371; // Radius of the Earth in kilometers
   const dLat = degreesToRadians(lat2 - lat1);
   const dLng = degreesToRadians(lng2 - lng1);
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(degreesToRadians(lat1)) * Math.cos(degreesToRadians(lat2)) *
-    Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    Math.cos(degreesToRadians(lat1)) *
+      Math.cos(degreesToRadians(lat2)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = R * c;
   return distance; // in kilometers
 }
-
