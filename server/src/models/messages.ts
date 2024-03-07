bd567de020fc5368ab317d9dcbce5994499956e9
Asiye Duman <asiyedumann@gmail.com>
2024-03-07 15:17:39 +0100

    feat: add Message model type

--- server/src/models/messages.ts
+++ server/src/models/messages.ts
@@ -1,7 +1,14 @@
 import mongoose from './index';
 
+type Message = {
+  message: string;
+  author: string; // user _id of sender
+  thread: string; // conversation _id which this message is about
+  read: boolean;
+  dateTime: Date;
+};
 // defining data structure
-const Message = new mongoose.Schema({
+const Message = new mongoose.Schema<Message>({
   message: String,
   author: String, // user _id of sender
   thread: String, // conversation _id which this message is about
