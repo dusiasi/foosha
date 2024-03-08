b5795b250c446707144721286e6f99d437f56257
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-08 10:09:56 +0100

    Conversations model

--- server/src/models/conversations.ts
+++ server/src/models/conversations.ts
@@ -1,7 +1,16 @@
 import mongoose from './index';
 
+type Conversation = {
+  itemName: string;
+  itemId: string;
+  itemImage: string;
+  contact: string;
+  owner: mongoose.Schema.Types.ObjectId;
+  date: Date;
+};
+
 // defining data structure
-const Conversation = new mongoose.Schema({
+const Conversation = new mongoose.Schema<Conversation>({
   itemName: String, // item name which this conversation is about
   itemId: String, // item _id which this conversation is about
   itemImage: String, // item image which this conversation is about
@@ -13,5 +22,5 @@ const Conversation = new mongoose.Schema({
   date: { type: Date, default: Date.now() }, // date of conversation start
 });
 
-const ConversationModel = mongoose.model('conversations', Conversation);
+const ConversationModel = mongoose.model<Conversation>('conversations', Conversation);
 export default ConversationModel;
