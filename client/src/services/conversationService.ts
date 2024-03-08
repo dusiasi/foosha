4a8be9e3d5ddb3c0b96cc8a4aa58093141545a44
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-08 18:18:41 +0100

    message service update

--- client/src/services/conversationService.ts
+++ client/src/services/conversationService.ts
@@ -3,20 +3,22 @@ import { Conversation, Item } from "../types";
 const rootUrl = `${import.meta.env.VITE_SERVER || 'http://localhost:4000'}/conversations`;
 
 
-export async function postConversation (body: Conversation) {
+export async function postConversation(body: Conversation): Promise<Conversation[]> {
   try {
     const response = await fetch(rootUrl, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  })
-  const data = await response.json();
-  return data;
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.log(error);
-}};
+    throw new Error("error posting conversation")
+  }
+};
 
 
 export async function getAllConversations ():Promise<Conversation[]> {
