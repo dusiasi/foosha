b6cc91ca8e1c7b6dd1de4b67434149a90ac330d8
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-09 11:56:09 +0100

    conversation service

--- client/src/services/conversationService.ts
+++ client/src/services/conversationService.ts
@@ -1,11 +1,10 @@
 import { Conversation, Item, User } from "../types";
-
 const rootUrl = `${
   import.meta.env.VITE_SERVER || "http://localhost:4000"
 }/conversations`;
-
-
-export async function postConversation(body: Conversation): Promise<Conversation> {
+export async function postConversation(
+  body: Conversation
+): Promise<Conversation> {
   try {
     const response = await fetch(rootUrl, {
       method: "POST",
@@ -21,7 +20,6 @@ export async function postConversation(body: Conversation): Promise<Conversation
     throw new Error("error posting conversation");
   }
 }
-
 export async function getAllConversations(): Promise<Conversation[]> {
   try {
     const response = await fetch(rootUrl, {
@@ -34,15 +32,11 @@ export async function getAllConversations(): Promise<Conversation[]> {
     throw new Error("error getting conversation");
   }
 }
-
 export async function getConversationByItemId(
   id: string,
   contact: User
 ): Promise<Conversation> {
   try {
-
-export async function getConversationByItemId (id:string, contact:Conversation):Promise<Conversation> {
-  try   {
     const response = await fetch(`${rootUrl}/${id}/${contact}`, {
       method: "GET",
     });
