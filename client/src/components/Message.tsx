961bf42532938f73db5ef5d2e005c308dfff9642
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-09 11:41:50 +0100

    messages

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -17,12 +17,12 @@ function Message({ item: item }: { item: MessageType}) {
       {item.author === user._id ? (
         <div className="message" id="message-outbound">
           <p className="message-text">{item.message}</p>
-          <p id="message-timestamp">{formatDateTime(item.dateTime)}</p>
+          <p id="message-timestamp">{formatDateTime(new Date(item.dateTime))}</p>
         </div>
       ) : (
         <div className="message" id="message-inbound">
           <p className="message-text">{item.message}</p>
-          <p id="message-timestamp">{formatDateTime(item.dateTime)}</p>
+          <p id="message-timestamp">{formatDateTime(new Date (item.dateTime))}</p>
         </div>
       )}
     </>
