0708d2028deeeeef37348fd2b24191d363714a8f
Asiye Duman <asiyedumann@gmail.com>
2024-03-09 14:03:16 +0100

    addform,error,conversation,messageservice update

--- client/src/routes/error-page.tsx
+++ client/src/routes/error-page.tsx
@@ -1,16 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
 import './error-page.css';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const error = useRouteError() as Error;
+
+  if (!isRouteErrorResponse(error)) {
+    return null;
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.data}</i>
       </p>
     </div>
   );
--- client/src/services/messageService.ts
+++ client/src/services/messageService.ts
@@ -1,32 +1,35 @@
-import { Message } from "../types";
+import { Message } from '../types';
 
-const rootUrl = `${import.meta.env.VITE_SERVER || 'http://localhost:3000'}/messages`;
+const rootUrl = `${
+  import.meta.env.VITE_SERVER || 'http://localhost:4000'
+}/messages`;
 
 export async function getAllMessages(): Promise<Message[]> {
   try {
     const response = await fetch(rootUrl, {
-      method: "GET",
+      method: 'GET',
     });
     const data = await response.json();
     return data;
   } catch (error) {
     console.log(error);
-    throw new Error ("error getting messages")
+    throw new Error('error getting messages');
   }
-};
+}
 
-export async function postMessage (body: Message):Promise<Message> {
+export async function postMessage(body: Message): Promise<Message> {
   try {
     const response = await fetch(rootUrl, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  })
-  const data = await response.json();
-  return data;
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.log(error);
-    throw new Error ("Error posting message")
-}};
+    throw new Error('Error posting message');
+  }
+}
